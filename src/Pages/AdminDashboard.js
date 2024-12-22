985d18e0b4735940150242b09c9b5e9d8bfc5266
Agustina Ortolachipi <agusortolachipi10@gmail.com>
2024-12-22 20:56:36 -0300

    Cree 3 archivos nuevos en la carpeta pages: administrador, inquilino, dueño de casa

---
+++ src/Pages/AdminDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      <h1>Panel de Administración</h1>
+      <p>Aquí puedes gestionar usuarios, propiedades y más.</p>
+    </div>
+  );
+};
+
+export default AdminDashboard;
---
+++ src/Pages/TenantArea.js
@@ -0,0 +1,13 @@
+// src/Pages/TenantArea.js
+import React from 'react';
+
+const TenantArea = () => {
+  return (
+    <div>
+      <h1>Bienvenido al Área de Inquilinos</h1>
+      <p>Aquí puedes buscar propiedades, ver tus favoritos y administrar tus datos.</p>
+    </div>
+  );
+};
+
+export default TenantArea;
